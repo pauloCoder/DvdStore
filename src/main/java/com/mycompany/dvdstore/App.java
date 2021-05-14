4b6e2b4852760ee0e575e561e3bc84e86d0544df
PauloCoder <williams56@hotmail.fr>
2021-05-14 23:48:51 +0200

    Déplacement des annotations vers la classe exécutable App.java

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -2,15 +2,23 @@ package com.mycompany.dvdstore;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import com.mycompany.dvdstore.controller.MovieController;
 
+@Configuration
+@ComponentScan(basePackages = {"com.mycompany.dvdstore.controller",
+							   "com.mycompany.dvdstore.service",
+							   "com.mycompany.dvdstore.repository.file"})
+@PropertySource(value = "classpath:application.properties") 
 public class App 
 {
     public static void main( String[] args )
     {
     	
-    	ApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+    	ApplicationContext context = new AnnotationConfigApplicationContext(App.class);
     	MovieController movieController = context.getBean(MovieController.class);
     	
         movieController.addUsingConsole();
